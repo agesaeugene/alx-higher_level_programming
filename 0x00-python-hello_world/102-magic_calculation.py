f6a8b6b4dc87dacebfecaaedfa23d5a7b4ebebeb
Eugene Agesa <eugenethebandit@gmail.com>
2024-06-22 02:43:29 -0700

    creating dataase and tables

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    result = 98
-    result += a ** b
-    return result
-
-# Example usage
-print(magic_calculation(2, 3))
+    return 98 + (a ** b)
---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+script to connect to a MySQL database and retrieve all rows from the 'states' table printing each row to the console
+"""
+import Mysqlclient
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+            passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    c = db.cursor()
+    c.execute("SELECT * FROM states")
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+    c.close()
+    db.close()
