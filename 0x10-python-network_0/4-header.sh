69f9fac287c56cebe7862542418d71d4989db397
Eugene Agesa <eugenethebandit@gmail.com>
2024-07-04 07:56:04 -0700

    cURL errors task 4 update

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # accepts an argument as a url, sends a get request to the url and displays the body of the response
 
-curl -sH "X-School-User-Id: 98" "$1"
+curl -sX "GET" -H 'X-School-User-Id: 98' "$1"
