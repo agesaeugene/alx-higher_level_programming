b26779084e4884474825adfe8ddceddbdbdf1aa6
Eugene Agesa <eugenethebandit@gmail.com>
2024-07-04 08:11:42 -0700

    cURL parameters update

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Acceps a URL, sends a Post Request and shows the response body
-curl -sX POST "$1" -d "email=test@gmail.com" -d "subject=I will always be here for PLD" $1
+curl -sX POST "$1" -d "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" 
