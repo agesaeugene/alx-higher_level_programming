3b2d024bf6dc618ee107121df2bed1eb2e052bc9
Eugene Agesa <eugenethebandit@gmail.com>
2024-07-04 19:14:57 -0700

    find peak task

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
+"""Defines a peak-finding algorithm to search for numbers."""
+
 
 def find_peak(list_of_integers):
-    """Identifies a peak in a list of unsorted integers."""
-    if not list_of_integers:
+    """Peak list of unsorted integers is returned."""
+    if list_of_integers == []:
         return None
 
-    left = 0
-    right = len(list_of_integers) - 1
+    size = len(list_of_integers)
+    if size == 1:
+        return list_of_integers[0]
+    elif size == 2:
+        return max(list_of_integers)
 
-    while left < right:
-        mid = left + (right - left) // 2
-        if list_of_integers[mid] < list_of_integers[mid + 1]:
-            left = mid + 1
-
-        else:
-            right = mid
-
-    return list_of_integers[left]
+    mid = int(size / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
