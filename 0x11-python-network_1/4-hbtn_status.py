f5ad4bf8671ff83edf19b283ac964fe9401b8308
Eugene Agesa <eugenethebandit@gmail.com>
2024-07-05 19:31:44 -0700

    python code to show my status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-# a get request to a given URL package is requested
+""" a get request to a given URL package is requested"""
 import requests
 
 if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
-    r = requests.get(url)
-    print("Body response:")
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    print('Body response:')
     print('\t- type:', type(r.text))
     print('\t- content:', r.text)
+
