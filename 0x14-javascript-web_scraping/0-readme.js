23fe74baca4f42507d2ae04348c13d5d21e107a2
Eugene Agesa <eugenethebandit@gmail.com>
2024-07-30 15:55:20 -0700

    added an error message to be printed

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -9,3 +9,11 @@ fs.readFile(process.argv[2], 'utf8', (err, data) => {
 		console.log(data);
 	}
 });
+
+const args = process.argv.slice(2);
+if (args.length !== 1) {
+    console.log('Usage: node script.js <file_path>');
+} else {
+    readFile(args[0]);
+}
+
